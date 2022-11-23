981b54f1376f649c4e99ae8ac54b95789d20f4b8
ErnestH1 <hansonernest7@gmail.com>
2022-11-23 09:24:33 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
 // Write your solution in this file!
+
+const employee = {
+    name: "Ernest Hansone",
+    streetAddress: "261 Riara"
+}
+
+
+function updateEmployeeWithKeyAndValue(obj, key, value){
+  return {
+    ...obj,
+    [key] : value
+  }
+}
+
+
+console.log(updateEmployeeWithKeyAndValue(employee, "workId", 340))
+console.log(employee);
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value){
+    obj[key] = value;
+    return obj;
+}
+
+
+destructivelyUpdateEmployeeWithKeyAndValue(employee, "workId", 250)
+console.log(employee);
+
+function deleteFromEmployeeByKey(obj, key){
+    const Nobj = {
+        ...obj
+    }
+    delete Nobj[key];
+    return Nobj;
+}
+
+
+console.log(deleteFromEmployeeByKey(employee, "workId"))
+console.log(employee);
+
+function destructivelyDeleteFromEmployeeByKey(obj, key){
+    delete obj[key];
+    return obj;
+}
+
+
+destructivelyDeleteFromEmployeeByKey(employee, "workId")
+console.log(employee);
